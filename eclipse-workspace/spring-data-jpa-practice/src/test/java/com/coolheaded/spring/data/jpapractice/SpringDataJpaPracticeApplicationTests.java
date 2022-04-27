b7c74bb9f843cd046d5f6ddbb282cfd92534df5b
raginee <raginee.kct@gmail.com>
2022-04-27 10:49:00 -0500

    second

---
+++ eclipse-workspace/spring-data-jpa-practice/src/test/java/com/coolheaded/spring/data/jpapractice/SpringDataJpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolheaded.spring.data.jpapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
